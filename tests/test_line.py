b2bdca2f25e5c233bf2cd0da13614348a37dac09
Travis <tleedepriest@gmail.com>
2024-09-08 20:32:07 -0400

    add more assert statements to line test

--- tests/test_line.py
+++ tests/test_line.py
@@ -17,7 +17,7 @@ class TurtleMock:
         self.goto_called_with.append((x, y))
 
 
-def test_line_draw():
+def test_line():
     # Create an instance of Line
     turtle = TurtleMock()
     #steps = 10
@@ -26,6 +26,24 @@ def test_line_draw():
     line = Line(start=start, end=end)
     line.set_coordinates()
     # Call the draw method
+    assert line.start.x == 0
+    assert line.start.y == 0
+    assert line.end.x == 100
+    assert line.end.y == 100
+    line.translate_x(10)
+    assert line.start.x == 10
+    assert line.start.y == 0
+    
+    assert line.end.x == 110
+    assert line.end.y == 100
+
+    line.translate_y(10)
+    assert line.start.x == 10
+    assert line.start.y == 10
+
+    assert line.end.x == 110
+    assert line.end.y == 110
+
     line.draw(turtle_instance=turtle)
 
     # Assertions
